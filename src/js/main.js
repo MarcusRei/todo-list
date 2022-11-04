617624eef9cb2aab90fb73f1b4694573d8f00e37
MarcusRei <marcusreineck@gmail.com>
2022-11-04 12:42:58 +0100

    Skapat mer styling, implementerat sorteringsknapp

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 let ListElement = document.getElementById("ongoing-tasks-container");
 let newTaskButton = document.getElementById("create-btn");
 let userInput = document.getElementById("input-field");
+let sortButton = document.getElementById("sort-btn");
 
 newTaskButton.addEventListener("click", createNewTask);
+sortButton.addEventListener("click", sortAlphabetically);
 
 let taskList = [
   { userInput: "Mata hunden", isCompleted: false, onList: false },
@@ -15,17 +17,20 @@ createHTML();
 function createHTML() {
   for (let i = 0; i < taskList.length; i++) {
     let taskElement = document.createElement("li");
+    let userText = document.createElement("p");
     let doneButton = document.createElement("button");
     let removeButton = document.createElement("button");
     removeButton.innerText = "Ta bort";
     removeButton.addEventListener("click", removeTask);
     doneButton.innerText = "Klar";
-    doneButton.className = "complete-task";
+    doneButton.className = "complete-task-btn";
 
     if (taskList[i].onList == false) {
-      taskElement.innerText = taskList[i].userInput;
+      userText.innerText = taskList[i].userInput;
+      userText.className = "ongoing-text";
       taskElement.className = "ongoing-task";
       removeButton.className = "remove-btn";
+      taskElement.appendChild(userText);
       taskElement.appendChild(removeButton);
       ListElement.appendChild(taskElement);
       taskElement.appendChild(doneButton);
@@ -42,6 +47,7 @@ function createHTML() {
 
       if (taskList[i].isCompleted === true) {
         taskElement.className = "completed-task";
+        userText.className = "completed-text";
         doneButton.innerText = "Ångra";
         doneButton.className = "undo-btn";
       }
@@ -53,8 +59,9 @@ function createHTML() {
 
       if (taskList[i].isCompleted === false) {
         taskElement.className = "ongoing-task";
+        userText.className = "ongoing-text";
         doneButton.innerText = "Klar";
-        doneButton.className = "complete-task";
+        doneButton.className = "complete-task-btn";
         doneButton.removeEventListener("click", makeOngoing);
         doneButton.addEventListener("click", completeTask);
       }
@@ -75,11 +82,31 @@ function createHTML() {
 function createNewTask() {
   if (userInput.value == "") {
     alert("Du måste skriva något i fältet!");
+  } else {
+    let task = {
+      userInput: userInput.value,
+      isCompleted: false,
+      onList: false,
+    };
+    taskList.push(task);
+    console.log(taskList);
+    userInput.value = "";
+
+    createHTML();
   }
+}
 
-  let task = { userInput: userInput.value, isCompleted: false, onList: false };
-  taskList.push(task);
+function sortAlphabetically() {
+  taskList.sort(function (x, y) {
+    let a = x.userInput.toUpperCase(),
+      b = y.userInput.toUpperCase();
+    return a == b ? 0 : a > b ? 1 : -1;
+  });
+
+  ListElement.innerHTML = "";
   console.log(taskList);
-
+  for (let i = 0; i < taskList.length; i++) {
+    taskList[i].onList = false;
+  }
   createHTML();
 }
